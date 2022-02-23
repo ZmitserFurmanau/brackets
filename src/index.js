9fe58ec32f18aacd57cc87c5e359f62b0aac9aaf
ZmitserFurmanau <zmitserfurmanau@gmail.com>
2022-02-23 18:08:04 +0200

    feat: function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bool = true;
+  let run = str.length;
+  const arr = [];
+  const res = bracketsConfig.join(',').split(',').join('');
+
+  for (let i = 0; i < res.length; i += 2) {
+    arr.push(res.slice(i, i + 2));
+  };
+  
+  while (bool) {
+    run = str.length;
+
+    for (let i = 0; i < arr.length; i++) {
+      str = str.replace(arr[i],'');
+    };
+
+    if (run === str.length) {
+      bool = false;
+    };
+  };
+  
+  return run === 0;
+};
